d79fb4d9ff76ed5078cb389510237f638f74c7d3
DrewBoschert <drew.r.boschert@gmail.com>
2023-09-13 16:10:18 -0400

    Customer Login Added

--- AtmBankingConsole/Program.cs
+++ AtmBankingConsole/Program.cs
@@ -11,5 +11,42 @@ JsonSerializerOptions joptions = new JsonSerializerOptions()
     WriteIndented = true
 };
 
+
+/*
+$ Enter Card Code : 1234<enter>
+$ Enter Pin Code  : 6789<enter>
+Then there should be a menu showing the customer what the options are for the ATM. Options should include:
+
+* Balance
+* Deposit
+* Withdraw
+* Transfer
+* Show Transactions
+
+*/
+
 HttpClient http = new HttpClient();
+
+var read = Console.ReadLine();
+
+Console.WriteLine("'b' = Balance");
+Console.WriteLine("'d' = Deposit");
+Console.WriteLine("'w' = Withdraw");
+Console.WriteLine("'t' = Transfer");
+Console.WriteLine("'st' = Show Transactions");
+
+Console.Write("Enter Card Code: ");
+
+var cardCode = Console.ReadLine();
+Console.WriteLine();
+
+Console.Write("Enter Pin Code: ");
+var pinCode = Console.ReadLine();
+// check for the Pin Code here
+
+
+
+
+
+
 
